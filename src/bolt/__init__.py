c5bf2d5628255292c8d41ec89239244b62614d7e
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-12-04 14:54:27 -0300

    add base_score

--- src/bolt/__init__.py
+++ src/bolt/__init__.py
@@ -5,8 +5,8 @@ from .tree import Tree
 def bolt():
     parser = ArgumentParser(description="Boost Learning Transpiler")
     parser.add_argument("INPUT",            help="Input JSON")
-    parser.add_argument("-o", "--output",   help="Output file (default: INPUT.c)", default=None)
-    parser.add_argument("-f", "--function", help="Function name (default: INPUT)", default=None)
+    parser.add_argument("-o", "--output",   help="Output file   (default: INPUT.c)", default=None)
+    parser.add_argument("-f", "--function", help="Function name (default: INPUT)",   default=None)
     args = parser.parse_args()
 
     mname = args.INPUT.split('.')[0]
@@ -19,10 +19,10 @@ def bolt():
     with open(args.INPUT) as f:
         model = load(f)
 
+    base_score    = float(model["learner"]["learner_model_param"]["base_score"])
     feature_types = [x if x != "i" else "bool" for x in model["learner"]["feature_types"]]
     feature_names = model["learner"]["feature_names"]
-
-    trees = model["learner"]["gradient_booster"]["model"]["trees"]
+    trees         = model["learner"]["gradient_booster"]["model"]["trees"]
 
     res  = "#include <stdbool.h>\n\n"
     res += "float {}({})\n{{".format(args.function, ", ".join(["{} {}".format(feature_types[i], fname) for i, fname in enumerate(feature_names)]))
@@ -33,7 +33,7 @@ def bolt():
             tree
         )
         res += t.gen()+"\n"
-    res += "\treturn {} + {};\n".format(0.5, " + ".join("w{}".format(i) for i in range(len(trees))))
+    res += "\treturn {} + {};\n".format(base_score, " + ".join("w{}".format(i) for i in range(len(trees))))
     res += "}}\n".format()
 
     with open(args.output, "w") as f:
